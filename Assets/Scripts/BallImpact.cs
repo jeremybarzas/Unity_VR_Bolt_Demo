ddda9479f80a928a792adcf9cc38190e52b6fa1b
Jeremy Barzas <jbarzas@gmail.com>
2019-09-17 15:11:10 -0500

    hitting the ball with controllers working

---
+++ Assets/Scripts/BallImpact.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallImpact : MonoBehaviour
+{
+    Rigidbody rb;
+    public float m_forceMultipler;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("terrain"))
+            return;
+
+        var force = col.gameObject.GetComponent<VelocityCalculator>().m_velocity;
+        rb.AddForceAtPosition((force * m_forceMultipler), transform.position, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/VelocityCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityCalculator : MonoBehaviour
+{
+    public Vector3 m_velocity;
+    Vector3 prevPos;
+    Vector3 curPos;
+    
+    void Start()
+    {
+        prevPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        curPos = transform.position;
+        m_velocity = (curPos - prevPos) / Time.deltaTime;
+        prevPos = transform.position;
+    }
+}
