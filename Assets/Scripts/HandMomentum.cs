316e177f43ed4b0d3271e8445248d582b8ec5eb9
Jeremy Barzas <jbarzas@gmail.com>
2019-09-17 10:34:03 -0500

    current progress

---
+++ Assets/Scripts/HandMomentum.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMomentum : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    { 
+        // this is gathered data from the stick
+        Vector3 momentum = rb.mass * rb.velocity;
+        var force = momentum * Time.deltaTime;
+
+        // this is going to be done on the ball
+        Vector3 tempStickTipPos = new Vector3(1, 1, 1);
+        rb.AddForceAtPosition(force, tempStickTipPos, ForceMode.Impulse);
+    }
+}
